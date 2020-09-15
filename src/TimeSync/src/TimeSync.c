af8abb264229b59f3ba12796447b48d52a00159b
IanWhitehead <ian.w@drinkworks.com>
2020-09-14 22:20:20 -0400

    TimeSync with Host Clock update functional

--- src/TimeSync/src/TimeSync.c
+++ src/TimeSync/src/TimeSync.c
@@ -12,47 +12,23 @@
 #include	<string.h>
 #include	<time.h>
 
-
-//#include "iot_config.h"
-
-/* FreeRTOS includes.. */
-
-//#include "FreeRTOS.h"
-//#include "task.h"
-
-//#include "application.h"
-
-/* AWS System includes. */
-//#include "iot_system_init.h"
-//#include "iot_logging_task.h"
-
-#if !AFR_ESP_LWIP
-//#include "FreeRTOS_IP.h"
-//#include "FreeRTOS_Sockets.h"
-#endif
-
-//#include "driver/uart.h"
-//#include "tcpip_adapter.h"
-//#include "shci.h"
 #include "bleInterface.h"
-//#include "bleFunction.h"
-//#include "espFunction.h"
-//#include "wifiFunction.h"
+#include "bleInterface_internal.h"
+#include "bleFunction.h"
 #include "app_logging.h"
 #include "nvs_utility.h"
-//#include "event_fifo.h"
-//#include "event_records.h"
-#include "sntp.h"
-//#include "../freertos/vendors/espressif/esp-idf/components/lwip/lwip/src/include/lwip/apps/sntp.h"
+//#include "sntp.h"
+#include "../../../../freertos/vendors/espressif/esp-idf/components/lwip/include/apps/sntp/sntp.h"
+#include "../../../../freertos/vendors/espressif/esp-idf/components/lwip/lwip/src/include/lwip/apps/sntp.h"
 
 #define	DELTA_TIME_THRESHOLD	5			/**< Time delta threshold, in seconds, to initiate synchronizing host clock */
+
 
 /*
  * @ brief	How frequently to check RTC synchronization, in seconds
  */
 #define	CHECK_SYNC_TIME		( ( ( (23 * 24) + 59 ) * 60 ) + 55 )
-
-#ifdef	WORKING
+//#define	CHECK_SYNC_TIME		( 60 )
 
 typedef	struct
 {
@@ -65,30 +41,40 @@ static _time_sync_t	time_sync =
 		.ntp_sync = false,
 		.last_sync = 0L,
 };
+
 /**
  * @brief	Date/Time structure used by host/SHCI
  */
 typedef	struct
 {
-	uint16_t year;
-	uint8_t month;
-	uint8_t date;
-	uint8_t hour;
-	uint8_t minute;
-	uint8_t second;
-	uint8_t	weekday;
+	uint16_t year;				/**< Year: e.g. 2020 */
+	uint8_t month;				/**< Month: 1 - 12 */
+	uint8_t date;				/**< Day of Month: 1 - 31 */
+	uint8_t hour;				/**< Hour: 0 - 23 */
+	uint8_t minute;				/**< Minute: 0 - 59 */
+	uint8_t second;				/**< Second: 0 - 59 */
+	uint8_t	weekday;			/**< Day of Week: 1 - 7, 1 = Monday */
+	uint8_t	fraction;			/**< Faction of second */
+	uint8_t	reason;				/**< Reason for update */
 } __attribute__ ((packed)) _host_time_t;
 
+/**
+ * @brief	SNTP Time Synchronization Callback
+ *
+ * This function is called when a time synchronization of the local clock to NTP server occurs.
+ *
+ * @param[in]	tv	Pointer to timeval structure, containing time value from NTP server
+ */
 static void sntp_sync_time_cb(struct timeval *tv)
 {
 	char buf[28];
-	struct tm *gm = gmtime( &tv->tv_sec );
+	struct tm *gm = gmtime( (time_t *)&tv->tv_sec );
 
 	strftime( buf, 28, "%Y-%m-%dT%H:%M:%SZ", gm);
 	IotLogInfo( "sntp_sync_time_cb: %s", buf);
 	time_sync.ntp_sync = true;
-
 }
+
 
 /**
  * @brief	Current Time update handler
@@ -101,10 +87,11 @@ static void sntp_sync_time_cb(struct timeval *tv)
  */
 static void vCurrentTimeUpdate( const void *pData, const uint16_t size )
 {
-	int err;
+	int err = 0;
 	struct timeval tv;
 
-	_host_time_t *htime = pData;
+	const _host_time_t *htime = pData;
+	_host_time_t	updateTime;
 	struct tm time;
 	time_t host_time;
 	time_t	delta_time;
@@ -115,8 +102,6 @@ static void vCurrentTimeUpdate( const void *pData, const uint16_t size )
 		err = gettimeofday( &tv, NULL);
 		if( 0 == err )
 		{
-			IotLogInfo( "ntp time = %d", tv.tv_sec );
-
 			/* periodically check host RTC with local clock */
 			if( ( 0L == time_sync.last_sync) || ( CHECK_SYNC_TIME < ( tv.tv_sec - time_sync.last_sync ) ) )
 			{
@@ -124,7 +109,7 @@ static void vCurrentTimeUpdate( const void *pData, const uint16_t size )
 
 				/* convert from host_time_t to tm */
 				time.tm_year = htime->year - 1900;
-				time.tm_mon = htime->month;
+				time.tm_mon = htime->month - 1;
 				time.tm_mday = htime->date;
 				time.tm_hour = htime->hour;
 				time.tm_min = htime->minute;
@@ -139,10 +124,6 @@ static void vCurrentTimeUpdate( const void *pData, const uint16_t size )
 				}
 				else
 				{
-					IotLogInfo( " host_time = %d", host_time );
-
-					IotLogInfo( " host_time = %d, ntp = %d", host_time, tv.tv_sec );
-
 					/* compute positive time delta */
 					if( host_time > tv.tv_sec )
 					{
@@ -150,13 +131,28 @@ static void vCurrentTimeUpdate( const void *pData, const uint16_t size )
 					}
 					else
 					{
-						delta_time = host_time - tv.tv_sec;
+						delta_time = tv.tv_sec - host_time;
 					}
+
+					IotLogInfo( " host_time = %d, ntp = %d, delta = %d", host_time, tv.tv_sec, delta_time );
 
 					/* If time delta exceeds threshold, update host RTC */
 					if( DELTA_TIME_THRESHOLD < delta_time )
 					{
 						IotLogInfo( "TODO: Update Host RTC" );
+						struct tm *gm = gmtime( (time_t *)&tv.tv_sec );
+
+						updateTime.year = gm->tm_year + 1900;
+						updateTime.month = gm->tm_mon + 1;
+						updateTime.date = gm->tm_mday;
+						updateTime.hour = gm->tm_hour;
+						updateTime.minute = gm->tm_min;
+						updateTime.second = gm->tm_sec;
+						updateTime.weekday = ( gm->tm_wday ? gm->tm_wday : 7 );		/* POSIX: day of week [0,6] (Sunday = 0) -> BLE [1,7] 1 = Monday */
+						updateTime.fraction = 0;
+						updateTime.reason = 0;
+
+						bleFunction_writeCharacteristic( CURRENT_TIME_HANDLE, (uint8_t *)&updateTime, sizeof( _host_time_t ) );
 					}
 				}
 			}
@@ -164,7 +160,6 @@ static void vCurrentTimeUpdate( const void *pData, const uint16_t size )
 	}
 }
 
-#endif
 
 /* ************************************************************************* */
 /* ************************************************************************* */
@@ -178,7 +173,6 @@ static void vCurrentTimeUpdate( const void *pData, const uint16_t size )
  */
 int getUTC( char * buf, size_t size)
 {
-#ifdef	WORKING
 	int err;
 	struct timeval tv;
 
@@ -191,9 +185,6 @@ int getUTC( char * buf, size_t size)
 		IotLogInfo( "getUTC: %s", buf);
 	}
 	return err;
-#else
-	return -1;
-#endif
 }
 
 /**
@@ -201,7 +192,6 @@ int getUTC( char * buf, size_t size)
  */
 void TimeSync_init( void )
 {
-#ifdef	WORKING
 	sntp_setoperatingmode(SNTP_OPMODE_POLL);
 	sntp_setservername(0, "pool.ntp.org");
 	sntp_init();
@@ -209,5 +199,4 @@ void TimeSync_init( void )
 
 	/* Register callback for Current Time update */
 	bleInterface_registerUpdateCB( eCurrentTimeIndex, &vCurrentTimeUpdate );
-#endif
 }
